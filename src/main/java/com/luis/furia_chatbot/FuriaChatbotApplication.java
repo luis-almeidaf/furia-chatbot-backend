872d7e3f4bc2c51a607cb031b86a7ca49e95f0d3
unknown <luis.almeida.faria@gmail.com>
2025-04-25 20:30:19 -0300

    feat: set up `docker-compese` and database connection

---
+++ src/main/java/com/luis/furia_chatbot/FuriaChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.luis.furia_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuriaChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuriaChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luis/furia_chatbot/FuriaChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luis.furia_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuriaChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
